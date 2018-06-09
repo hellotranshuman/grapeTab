d920a5bdd1292253f4d11f6de0499f71a1aaf500
LeeJunyoung <hellotranshuman@gmail.com>
2018-06-09 20:37:52 +0900

    source code 1.0.0 	- create project

---
+++ app/js/popup.js
@@ -0,0 +1,76 @@
+// 메인 이미지 6개에 대응하는 span 태그 id 이름의 배열
+var mainImageTitleName = ['listName', 'deleteName', 'lockName', 'saveName', 'folderName', 'setupName'];
+
+// 메인 이미지 6개를 드래그 하지 못하도록 하는 함수
+function mainImageDragSet(){
+    for(var iCount = 0; iCount < mainImageName.length; iCount++){
+        // draggable 라는 속성을 false로 설정합니다.
+        $("#" + mainImageName[iCount]).prop('draggable', false);        
+    }
+};
+
+// messages.json에 저장된 기능 설명값을 저장하는 함수
+// function setMainImageTitleName(){
+//     // common.js에 선언되어 있는 setFunctionName() 함수를 호출하여 주요 기능의 이름값이 저장된 배열을 반환받습니다.
+//     var getFunctionNameArr = setFunctionName();
+
+//     for(var iCount = 0; iCount < mainFunctionName.length; iCount++){        
+//         $("#" + mainImageTitleName[iCount]).prop('innerHTML', getFunctionNameArr[iCount]);
+//     }
+// };
+
+// 클릭하여 이동할 페이지 주소를 설정하는 함수
+function setMovePage(){
+    // select-area를 class로 가지는 모든 엘리먼트를 배열로 대입합니다.
+    var getSelectAreaArr = $('.select-area');
+
+    // 클릭이벤트를 추가합니다.
+    getSelectAreaArr.each(function(index){
+        $(this).on('click', function(){
+            document.location.href = "app/html/view.html?page=" + index;
+        });
+    });    
+}
+
+// 마우스가 이미지의 위에 있을 경우 이미지를 변경시키는 이벤트를 추가하는 함수
+function changeImg(){
+    // select-area를 class로 가지는 모든 엘리먼트를 배열로 대입합니다.
+    var getSelectAreaArr = $('.select-area');
+
+    // 마우스 이벤트를 추가합니다.
+    getSelectAreaArr.each(function(index){
+        // 이미지가 변경되기전 src값을 구합니다.
+        var beforeImgName = $('#' + mainImageName[index]).attr('src');
+        // 변경될 이미지의 src값을 구합니다.
+        var afterImgName = $('#' + mainImageName[index]).attr('src').split('.')
+        afterImgName = afterImgName[0] + '_change.png';
+        
+        // jQuery로 이미지 엘리먼트를 구합니다.
+        var $imgObj = $('#' + mainImageName[index]);
+
+        // 마우스가 위에 있을때 실행 될 이벤트를 추가합니다.
+        $(this).on('mouseover', function(){
+            // console.log('----- over -----' + index);
+            $imgObj.attr('src', afterImgName);
+        });
+
+        // 마우스가 위에서 벗어 났을때 실행 될 이벤트를 추가합니다.
+        $(this).on('mouseout', function(){
+            // console.log('&&&&& out &&&&&' + index);
+            $imgObj.attr('src', beforeImgName);
+        });
+    });
+}
+
+// DOM이 완전히 로드되면 이벤트 리스너를 추가합니다.
+document.addEventListener('DOMContentLoaded', function(){
+    // 메인 이미지 6개를 드래그 하지 못하도록 하는 함수
+    mainImageDragSet();
+    // common.js에 선언되어 있는 setFunctionName() 함수를 호출하여 주요 기능의 이름값이 저장된 배열을 반환받습니다.
+    setFunctionName(mainImageTitleName);
+    // 마우스가 이미지의 위에 있을 경우 이미지를 변경시키는 이벤트를 추가하는 함수
+    changeImg();
+    // 클릭하여 이동할 페이지 주소를 설정하는 함수
+    setMovePage();
+
+});
---
+++ app/js/view.js
@@ -0,0 +1,64 @@
+// 메인 이미지 6개에 대응하는 span 태그 id 이름의 배열
+var navBarNameArr = ['listNav', 'deleteNav', 'lockNav', 'saveNav', 'folderNav', 'setupNav'];
+
+// 네비게이션바에 click에 따른 페이지이동 이벤트를 추가하는 함수
+function moveContentPage() {
+    // contentArea를 id로 가는 엘리먼트를 구합니다.
+    var $contentArea = $('#contentArea');
+
+    // jQuery로 네비게이션 바의 Element를 찾는데 사용될 문자열이 저장될 배열
+    var setNavNameStr = '';
+
+    // jQuery로 네비게이션 바의 Element를 찾는데 사용될 문자열을 저장합니다.
+    // 가장 마지막의 경우 , 를 붙이지 않도록 합니다. (jQuery문법상의 이유)
+    for(var iCount = 0; iCount < navBarNameArr.length; iCount++){
+        if(iCount == navBarNameArr.length - 1){
+            setNavNameStr += '#' + navBarNameArr[iCount];
+        } else {
+            setNavNameStr += '#' + navBarNameArr[iCount] + ',';
+        }
+    }
+
+    // 네비게이션 바의 원소를 jQuery형 배열로 대입합니다.
+    navAreaArr = $(setNavNameStr);
+
+    // 네비게이션바에 해당하는 페이지로 이동하는 click이벤트를 추가합니다.
+    navAreaArr.each(function(index){
+        $(this).on('click', function(){
+            $contentArea.attr('src', './' + mainImageName[index] + '.html');
+        });
+    });   
+}
+
+// popup.html에서 선택한 것에 맞게 view.html 페이지가 출력되도록 하는 함수
+// 1. 전달된 파라미터 획득, 2. 파라미터 값에 해당하는 네비게이션바 및 iframe src 설정
+function setNavMovePage(){  
+    var sendParameter = null;
+    // DOM이 모두 생성되면 실행됩니다.
+    $(document).ready(function(){
+        // 참고 전달받은 값 내역 [전달받을 값 : 나타내는 페이지] 
+        // -> [0:list], [1:delete], [2:lock], [3:save], [4:folder], [5:setup]
+        var getParameter = location.href.substr(
+            location.href.lastIndexOf('=') + 1
+        );
+
+        // popup.js에서 선택한 것에 해당하는 네비게이션바가 활성화되도록 class값을 변경합니다.
+        $('#' + navBarNameArr[getParameter]).attr('class', 'nav-item nav-link active');
+
+        // contentArea를 id로 가는 엘리먼트를 구한 다음
+        // popup.js에서 선택한 것에 맞추어 view.html의 iframe에 출력할 페이지 url을 변경합니다.
+        $('#contentArea').attr('src', './' + mainImageName[getParameter] + '.html');
+    });
+}
+
+// DOM이 완전히 로드되면 이벤트 리스너를 추가합니다.
+document.addEventListener('DOMContentLoaded', function(){
+    // common.js에 선언되어 있는 setFunctionName() 함수를 호출하여 주요 기능의 이름값이 저장된 배열을 반환받습니다.
+    setFunctionName(navBarNameArr);
+
+    // 네비게이션바에 click에 따른 페이지이동 이벤트를 추가하는 함수
+    moveContentPage();
+    // popup.html에서 선택한 것에 맞게 view.html 페이지가 출력되도록 하는 함수
+    setNavMovePage();
+
+});
